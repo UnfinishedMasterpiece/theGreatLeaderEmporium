e2dc33af4a625c32a4727de37a13355fc4bc120f
Sergio Flor <36941072+UnfinishedMasterpiece@users.noreply.github.com>
2018-04-12 18:33:00 -0400

    Add files via upload

---
+++ js/cookies.js
@@ -0,0 +1,27 @@
+/*
+	Final Project
+	File Name: cookies.js
+	Author: Sergio Flor
+*/
+function setCookie(cName, cValue, expDate, cPath, cDomain, cSecure) {
+   if (cName && cValue != "") {
+      var cString = cName + "=" + escape(cValue);
+      cString += (expDate ? ";expires=" + expDate.toGMTString(): "");
+      cString += (cPath ? ";path=" + cPath : "");
+      cString += (cDomain ? ";domain=" + cDomain : "");
+      cString += (cSecure ? ";secure" : "");
+      document.cookie = cString;
+   }
+}
+
+
+function getCookie(cName) {
+   if (document.cookie) {
+      var cookies = document.cookie.split("; ");
+      for (var i = 0; i < cookies.length; i++) {
+         if (cookies[i].split("=")[0] == cName) {
+            return unescape(cookies[i].split("=")[1]);
+         }
+      }
+   }
+}
---
+++ js/shoppingCart.js
@@ -0,0 +1,73 @@
+/*
+	Final Project
+	File Name: shoppingCart.js
+	Author: Sergio Flor
+	
+	Variable List:
+	
+	productShortName: Product ID
+	productLongName: Product Name
+	productThumbNailURL: Thumbnail Image URL 
+	productLargeImgURL: URL for large Image page
+	productShortDescription: Short Desc.
+	productDetailedDescription: Full Desc.
+	productPrice: Price $9.95-$49.95
+
+*/
+
+var productShortName = new Array();
+var productLongName = new Array();
+var productThumbNailURL = new Array();
+var productLargeImgURL = new Array();
+var productShortDescription = new Array();
+var productDetailedDescription = new Array();
+var productPrice = new Array();
+
+productShortName[0] = "generalPatton";
+productShortName[1] = "georgeWashington";
+productShortName[2] = "hannibalBarca";
+productShortName[3] = "kimJongUn";
+productShortName[4] = "maoZedong";
+productShortName[5] = "thisMothertruckingFringleDongle";
+
+productLongName[0] = "General George Patton";
+productLongName[1] = "George Washington";
+productLongName[2] = "Hannibal Barca";
+productLongName[3] = "Kim Jong Un";
+productLongName[4] = "Mao Zedong";
+productLongName[5] = "Donald Trump";
+
+productThumbNailURL[0] = "img/generalPatton.jpg";
+productThumbNailURL[1] = "img/georgeWashington.jpg";
+productThumbNailURL[2] = "img/hannibalBarca.jpg";
+productThumbNailURL[3] = "img/kimJongUn.jpg";
+productThumbNailURL[4] = "img/maoZedong.jpg";
+productThumbNailURL[5] = "img/thisMothertruckingFringleDongle.jpg";
+
+productLargeImgURL[0] = "generalPatton.html";
+productLargeImgURL[1] = "georgeWashington.html";
+productLargeImgURL[2] = "hannibalBarca.html";
+productLargeImgURL[3] = "kimJongUn.html";
+productLargeImgURL[4] = "maoZedong.html";
+productLargeImgURL[5] = "thisMothertruckingFringleDongle.html";
+
+productShortDescription[0] = "Are your armies revolting against your government? General George Patton will slap them into shape!";
+productShortDescription[1] = "Is your nation still in its infancy? Need someone to steer your nation clear of disaster? George Washington will command your nation to independence!";
+productShortDescription[2] = "Is the cultural state of your nation hanging in the balance due to Rome being such a meanie? Then Hannibal Barca is the guy for you! He whooped Rome so hard that they just had to include him in their history!";
+productShortDescription[3] = "Kim Jong Un has an explosive personality. He likes to drink margaritas and listen to Katy Perry all the time. He'll treat your country so well that it will become part of his nuclear family!";
+productShortDescription[4] = "Have 5 years to spare? Once your nation is stabilized, you'll need a strong leader to keep your nation stable!";
+productShortDescription[5] = "What!? Seriously!? This, m************ a******?!";
+
+productDetailedDescription[0] = "George Smith Patton, Jr. (November 11, 1885 – December 21, 1945) was a senior officer of the United States Army, who commanded the U.S. Seventh Army in the Mediterranean and European Theaters of World War II, but is best known for his leadership of the U.S. Third Army in France and Germany following the Allied invasion of Normandy in June 1944.";
+productDetailedDescription[1] = "George Washington (February 22, 1732 [O.S. February 11, 1731] – December 14, 1799) was the first President of the United States (1789–97), the Commander-in-Chief of the Continental Army during the American Revolutionary War, and one of the Founding Fathers of the United States. He presided over the convention that drafted the current United States Constitution and during his lifetime was called the father of his country.";
+productDetailedDescription[2] = "Hannibal (247 - ~183-~181 bc),fully Hannibal Barca, was a Punic Carthaginian military commander, generally considered one of the greatest military commanders in history.";
+productDetailedDescription[3] = "Kim Jong-un (Korean pronunciation: [kimd͜zɔŋɯn]; born 8 January 1983; also romanized as Kim Jong-eun, Kim Jong Un or Kim Jung-eun) is the supreme leader of the Democratic People's Republic of Korea (DPRK), commonly referred to as North Korea.";
+productDetailedDescription[4] = "Mao Zedong, also transliterated as Mao Tse-tung and commonly referred to as Chairman Mao (December 26, 1893 – September 9, 1976), was a Chinese Communist revolutionary and the founding father of the People's Republic of China, which he governed as Chairman of the Communist Party of China from its establishment in 1949 until his death in 1976.";
+productDetailedDescription[5] = "Donald John Trump (born June 14, 1946) is an American business magnate, investor, author, television personality and candidate for President of the United States in the 2016 presidential election. He is the chairman and president of The Trump Organization, and the founder of Trump Entertainment Resorts. Trump's career, branding efforts, lifestyle and outspoken manner helped make him a celebrity, a status amplified by the success of his NBC reality show, The Apprentice.";
+
+productPrice[0] = "49.95";
+productPrice[1] = "45.00";
+productPrice[2] = "30.00";
+productPrice[3] = "15.00";
+productPrice[4] = "20.00";
+productPrice[5] = "0.01";
